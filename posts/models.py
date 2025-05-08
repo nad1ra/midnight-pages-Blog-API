c588a9b29b72dead97ec41bc8d4c84fe41afa32d
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-05-08 19:41:32 +0500

    post created

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.base_models import BaseModel
 
-# Create your models here.
+
+class Post(BaseModel):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Like(models.Model):
+    pass
